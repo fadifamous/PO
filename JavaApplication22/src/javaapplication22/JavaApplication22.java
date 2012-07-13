d919a6f028fb163f548a0cf735a9a837f56d3aee
FFK <FFK@FFK-THINK>
2012-07-13 11:25:25 +0300

    Trying to add audio files ...

---
+++ JavaApplication22/src/javaapplication22/JavaApplication22.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication22;
+
+/**
+ *
+ * @author FFK
+ */
+public class JavaApplication22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("test");
+        // TODO code application logic here
+    }
+}
